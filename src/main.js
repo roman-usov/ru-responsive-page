14a9bde37d439901a62624e63e7bb5fc62d4a9c7
roman-usov <66104246+roman-usov@users.noreply.github.com>
2022-12-17 21:11:17 +0400

    added mobile header and hero

---
+++ src/main.js
@@ -0,0 +1,26 @@
+const headerToggleEl = document.querySelector(".header__toggle");
+const navigationEl = document.querySelector(".navigation");
+const headerHamburgerEl = document.querySelector(".header__hamburger");
+
+const openMobileNavMenu = () => {
+  navigationEl.classList.remove("navigation_visibility_invisible");
+  navigationEl.classList.add("navigation_visibility_visible");
+  headerHamburgerEl.classList.add("header__hamburger_opened");
+};
+
+const closeMobileNavMenu = () => {
+  navigationEl.classList.remove("navigation_visibility_visible");
+  navigationEl.classList.add("navigation_visibility_invisible");
+  headerHamburgerEl.classList.remove("header__hamburger_opened");
+};
+
+headerToggleEl.addEventListener("click", () => {
+  const isClosed = navigationEl.classList.contains(
+    "navigation_visibility_invisible"
+  );
+  if (isClosed) {
+    openMobileNavMenu();
+  } else {
+    closeMobileNavMenu();
+  }
+});
